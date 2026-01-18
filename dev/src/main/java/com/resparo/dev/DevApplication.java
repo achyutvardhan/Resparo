17969e1b8509f8237c4f77d3365a045b7adce507
achyutvardhan <achyutvardhan234@gmail.com>
2026-01-19 01:32:49 +0530

    first command

---
+++ dev/src/main/java/com/resparo/dev/DevApplication.java
@@ -0,0 +1,15 @@
+package com.resparo.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
+
+@SpringBootApplication
+@CommandScan
+public class DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+	}
+
+}
---
+++ dev/src/main/java/com/resparo/dev/Mycommand.java
@@ -0,0 +1,15 @@
+package com.resparo.dev;
+
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.command.annotation.Option;
+import org.springframework.stereotype.Component;
+
+@Component
+@Command(group = "My Commands")
+public class Mycommand {
+    
+    @Command(description = "Prints Hello, World!")
+    public String hello(@Option(description = "Name to greet", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
+}
